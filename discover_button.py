f7fedaf3d475d0ad6d2da3be49f4384793974cd0
Eric Rosen <ericrosen@Erics-MBP-4.T-mobile.com>
2020-04-19 01:10:29 -0400

    normalized object-object-dynamic dicts

--- discover_button.py
+++ discover_button.py
@@ -257,145 +257,28 @@ while looking:
 
 viz_ichain(random_ichain)
 
-#compute how likely this graph is to exist
-#p stands for probability
-p_robot_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-p_u_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-p_r_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-p_d_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-p_l_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-game_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-game_robot_dict = {}
+###compute how likely this graph is to exist
 
-#update p_robot_dict
-for pos,pod in robot_dict.items(): #for each position robot can take on
-    for obj,objd in pod.items(): #for each potential successor object
-        total_counts = 0
-        for dyn,counts in objd.items(): #for each dynamic successor objecy can take on
-            total_counts += counts
-        for dyn,counts in objd.items(): #now populate p_robot_dict with probabilities
-            p_robot_dict[pos][obj][dyn] = float(counts)/total_counts
+def dict_to_prob(d):
+    #normalize values in object-object-dynamic dict
+    ret_d = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+    for pos,pod in d.items(): #for each position robot can take on
+        for obj,objd in pod.items(): #for each potential successor object
+            total_counts = 0
+            for dyn,counts in objd.items(): #for each dynamic successor objecy can take on
+                total_counts += counts
+            for dyn,counts in objd.items(): #now populate p_robot_dict with probabilities
+                ret_d[pos][obj][dyn] = float(counts)/total_counts
+    return(ret_d)
+
+p_robot_dict = dict_to_prob(robot_dict)
+p_u_dict = dict_to_prob(u_dict)
+p_d_dict = dict_to_prob(d_dict)
+p_l_dict = dict_to_prob(l_dict)
+p_r_dict = dict_to_prob(r_dict)
+p_game_dict = dict_to_prob(game_dict)
         
-
-#update p_u_dict
-for pos,pod in u_dict.items(): #for each position obj can take on
-    for obj,objd in pod.items(): #for each potential successor object
-        total_counts = 0
-        for dyn,counts in objd.items(): #for each dynamic successor objecy can take on
-            total_counts += counts
-        for dyn,counts in objd.items(): #now populate p_robot_dict with probabilities
-            p_u_dict[pos][obj][dyn] = float(counts)/total_counts
-print(robot_dict[(1,1)]['u'])
 print(p_robot_dict[(2,4)]['game'])
-print(u_dict[True]['d'])
-print(u_dict[False]['l'])
+print(p_u_dict[True]['d'])
+print(p_u_dict[False]['l'])
 
-#Code for discovering structures
-objs = ["robot","u","d","l","r","game"]
-obj_dyn ={"u": ["u_press"], "d": ["d_press"], "l": ["l_press"], "r":["r_press"], "game":["up","right","down","left"]}
-
-true_dyn_succ = {"u_press": "robot", "d_press": "robot", "l_press": "robot", "r_press": "robot", "up" : "u", "right": "r", "down": "d", "left": "l"}
-
-def random_dynamic_successor(obj_dyn):
-    ret_graph = {}
-    graph_objects = list(obj_dyn.keys())
-    for obj in graph_objects:
-        potential_successors = copy.deepcopy(graph_objects)
-        potential_successors.remove(obj)
-        potential_successors.append("robot")
-        for dyn in obj_dyn[obj]:
-            ret_graph[dyn] = random.choice(potential_successors)
-    return(ret_graph)
-
-print(random_dynamic_successor(obj_dyn))
-
-#turn random_graph into a graph based on objct dynamics, if it is a DAG, valid. Otherwise, throwout
-
-def create_ichain(dyn_succ, obj_dyn):
-    #create a graph based on objects, dynamics, and succesor relationships, and check if it's valid
-    obj_nodes = list(obj_dyn.keys()) + ["robot"]
-    dyn_nodes = list(dyn_succ.keys())
-    nodes = obj_nodes + dyn_nodes
-    num_nodes = len(nodes)
-
-    #generate the graph and add edges according to succesor relationship and obj_dynk
-    #g = Graph(num_nodes)
-    G = nx.DiGraph()
-
-    #add directed edges from object dynamic to associated object (obj_dyn) (except for robot)
-    for obj in obj_dyn.keys():
-        for dyn in obj_dyn[obj]:
-            G.add_edge(dyn,obj)
-  
-    #add directed edges from successor objects to dynamics (dyn_succ)
-    for dyn in dyn_succ.keys():
-        G.add_edge(dyn_succ[dyn],dyn)
-
-    return(G)
-
-def viz_ichain(G):
-    #draw graph for sanity
-    positions = nx.spring_layout(G)
-
-    nx.draw_networkx_labels(G,positions)
-    nx.draw_networkx_edges(G,positions)
-    nx.draw_networkx_nodes(G,positions)
-    plt.show()
-
-    
-
-true_ichain = create_ichain(true_dyn_succ,obj_dyn)
-print(nx.is_directed_acyclic_graph(true_ichain))
-#viz_ichain(true_ichain)
-
-looking = True
-while looking:
-    random_dyn_succ = random_dynamic_successor(obj_dyn)
-    random_ichain = create_ichain(random_dyn_succ,obj_dyn)
-    if nx.is_directed_acyclic_graph(random_ichain):
-        looking = False
-
-viz_ichain(random_ichain)
-
-#compute how likely this graph is to exist
-#p stands for probability
-p_robot_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-p_u_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-p_r_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-p_d_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-p_l_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-game_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
-game_robot_dict = {}
-
-#update p_robot_dict
-for pos,pod in robot_dict.items(): #for each position robot can take on
-    for obj,objd in pod.items(): #for each potential successor object
-        total_counts = 0
-        for dyn,counts in objd.items(): #for each dynamic successor objecy can take on
-            total_counts += counts
-        for dyn,counts in objd.items(): #now populate p_robot_dict with probabilities
-            p_robot_dict[pos][obj][dyn] = float(counts)/total_counts
-        
-
-#update p_u_dict
-for pos,pod in u_dict.items(): #for each position obj can take on
-    for obj,objd in pod.items(): #for each potential successor object
-        total_counts = 0
-        for dyn,counts in objd.items(): #for each dynamic successor objecy can take on
-            total_counts += counts
-        for dyn,counts in objd.items(): #now populate p_robot_dict with probabilities
-            p_u_dict[pos][obj][dyn] = float(counts)/total_counts
-print(robot_dict[(1,1)]['u'])
-print(p_robot_dict[(2,4)]['game'])
-
-
-
-#demonstrate how to convert this into transition function (Hint, it's really easy, just normalize the above probabilities and sample from them when applying update dynamics according to object_dynamics, and make sure that in order to avoid active dynamic conflicts to resolve the object update only when all dynamic likelihoods have been calculated, and choose most likely)
-#use transition function in VI
-#plug entire thing together and look at results!
-
-
-
-#demonstrate how to convert this into transition function (Hint, it's really easy, just normalize the above probabilities and sample from them when applying update dynamics according to object_dynamics, and make sure that in order to avoid active dynamic conflicts to resolve the object update only when all dynamic likelihoods have been calculated, and choose most likely)
-#use transition function in VI
-#plug entire thing together and look at results!
